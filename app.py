21c54f7a3b9c6474fccb4226ee4f42642a74175a
RamSuren <suren.coool@gmail.com>
2021-04-22 18:32:32 +0530

    added post and get methods

--- app.py
+++ app.py
@@ -1,33 +1,56 @@
-from datetime import datetime
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
-db = SQLAlchemy(app)
+
+agents = []
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True, nullable=False)
-    email = db.Column(db.String(130), unique=True, nullable=False)
-    image = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    details = db.relationship('Detail', backref='surname', lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image}')"
+@app.route('/agent/<string:name>')
+def get_agent(name):
+    for agent in agents:
+        if agent['name'] == name:
+            return jsonify(agent['name'])
+    return jsonify({'message': 'Agent not found'})
 
 
-class Detail(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    dob = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Detail('{self.name}', '{self.age}', '{self.surname}', '{self.dob}')"
+@app.route('/agent/<string:name>/detail')
+def get_agent_item(name):
+    for agent in agents:
+        if agent['name'] == name:
+            return jsonify(agent['details'])
+    return jsonify({'message': 'Agent not found'})
 
 
+@app.route('/agent', methods=['POST'])
+def create_agent():
+    req_data = request.get_json()
+    new_agent = {
+        'name': req_data['name'],
+        'details': req_data['details']
+    }
+    agents.append(new_agent)
+    return jsonify(new_agent)
 
+
+@app.route('/agent/<string:name>/detail', methods=['POST'])
+def create_agent_detail(name):
+    for agent in agents:
+        if agent['name'] == name:
+            req_data = request.get_json()
+            new_detail = {
+                'age': req_data['age'],
+                'surname': req_data['surname'],
+                'dob': req_data['dob']
+
+            }
+            agent['detail'].append(new_detail)
+            return jsonify(new_detail)
+        return jsonify({'message': 'Agent not found'})
+
+
+@app.route('/')
+def home():
+    return "hey"
+
+
+app.run(port=8000)
